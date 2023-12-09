f57e18ab5b430288d2f5078fb2d25c586e992e37
DESKTOP-GQF1ESP\User <linyier1027@gmail.com>
2023-12-09 16:14:13 +0800

    抓第一欄

--- spy3.py
+++ spy3.py
@@ -10,6 +10,25 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import time
-from os.path import exsits
+from os.path import exists
 
 html=requests.get("https://rate.bot.com.tw/xrt?Lang=zh-TW")
+bsObj=BeautifulSoup(html.content,"lxml")
+file_name="E2-1-1-1"+"各國貨幣"+".csv"
+
+for single_tr in bsObj.find("table",{"title":"牌告匯率"}).find("tbody").findAll("tr"):
+    cell=single_tr.findAll("td")
+    currency_name=cell[0].find("div",{"class":"visible-phone"}).contents[0]
+    currency_name=currency_name.replace("\r","")
+    currency_name=currency_name.replace("\n","")
+    currency_name=currency_name.replace(" ","")
+    rate1=cell[1].contents[0]
+    rate2=cell[2].contents[0]
+    rate3=cell[3].contents[0]
+    rate4=cell[4].contents[0]
+    
+    print(currency_name,rate1)
+    
+
+
+    
