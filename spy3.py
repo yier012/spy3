fa2983a163ac6474c54a2418a07ef6ad2c5b36ef
DESKTOP-GQF1ESP\User <linyier1027@gmail.com>
2023-12-09 16:05:51 +0800

    連結台銀牌告匯率網頁

--- spy3.py
+++ spy3.py
@@ -5,4 +5,11 @@ Created on Sat Dec  9 14:31:49 2023
 @author: User
 """
 
-000
+##########台銀牌告匯率擷取##########
+import requests
+from bs4 import BeautifulSoup
+import csv
+import time
+from os.path import exsits
+
+html=requests.get("https://rate.bot.com.tw/xrt?Lang=zh-TW")
